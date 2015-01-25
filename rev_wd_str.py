1d58d1a92f8662a3e833074a1489e2c755324f64
Sophia <claraqqqq@gmail.com>
2015-01-24 21:06:34 -0700

    initial commit

---
+++ rev_wd_str.py
@@ -0,0 +1,11 @@
+# Reverse Words in a String
+# Given an input string, reverse the string word by word.
+# For example,
+# Given s = "the sky is blue",
+# return "blue is sky the".
+
+class Solution:
+    # @param s, a string
+    # @return a string
+    def reverseWords(self, s):
+        return ' '.join(s.split()[::-1])

4a36bdb1f918af8fa4825e1bdcc69598dcd87888
sophia <claraqqqq@gmail.com>
2015-10-05 21:12:04 -0600

    fix code

--- sqrt.py
+++ sqrt.py
@@ -1,22 +1,16 @@
-# Sqrt(x)
-# Implement int sqrt(int x).
-# Compute and return the square root of x.
-
-class Solution:
-	# @param x, an integer
-	# @return an integer
-	def sqrt(self, x):
-		high = x
-		low = 0
-		mid = high
-		label = True
-		while label:
-			if mid ** 2 <= x and (mid+1) ** 2 > x:
-				label = False
-				continue
-			elif mid ** 2 < x:
-				low = mid
-			elif mid ** 2 > x:
-				high = mid
-			mid = int((high + low) / 2)
-		return mid
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        left = 0
+        right = 46340 # sqrt(C MAX_INT 2147483647)=46340.950001
+        while left <= right:
+            mid = (left + right) / 2
+            if mid ** 2 <= x < (mid + 1) ** 2:
+                return mid
+            elif mid ** 2 > x:
+                right = mid - 1
+            else:
+                left = mid + 1
